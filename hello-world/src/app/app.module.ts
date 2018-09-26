62c370a27bc169d42801d567f4a2894772422e24
Saravana Prasanth Natarajan <srvnprasanth@gmail.com>
2018-09-26 23:21:39 +0530

    Course Form Assingment
    refs #ticket-saravana Course Form Assingment workout commit..!
    
    Co-Authored-By: srvnprasanth <srvnprasanth@users.noreply.github.com>

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { CourseFormComponent } from './course-form/course-form.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     LikeComponent,
     InputFormatDirective,
     ZippyComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    CourseFormComponent
   ],
   imports: [
     BrowserModule, FormsModule
