8c1cfeb362e2cb864c18f40a3b8bb8b3f989269b
Saravana Prasanth Natarajan <srvnprasanth@gmail.com>
2018-10-07 22:09:44 +0530

    Authentication & Authorization
    refs #ticket-saravana Upto CanActivated Interface workout commit..!
    
    Co-Authored-By: srvnprasanth <srvnprasanth@users.noreply.github.com>

---
+++ auth-demo/src/app/services/auth.service.ts
@@ -0,0 +1,49 @@
+import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthService {
+  constructor(private http: Http) {
+  }
+
+  login(credentials) { 
+   return this.http.post('/api/authenticate', 
+      JSON.stringify(credentials))
+      .map( response =>{
+        // console.log(response.json());
+        let result = response.json();
+        if(result && result.token){
+          localStorage.setItem('token',result.token);
+          return true;
+        }
+        return false;
+      });
+  }
+
+  logout() { 
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn() {
+    // let jwtHelper  = new JwtHelper;
+    // let token = localStorage.getItem('token');
+
+    // if(!token)
+    //   return false;
+    // let isExpired = jwtHelper.isTokenExpired(token);
+    //   return !isExpired;
+
+    // Instead of this we can use gloabal function
+
+    return tokenNotExpired();
+  }
+
+  get userDetails(){
+    let token = localStorage.getItem('token');
+    if (!token) return null;
+    return new JwtHelper().decodeToken(token);
+  }
+}
+
